452ac512d6e70d9774c774c9793bfa42dfdbad8d
anchart <valenanchart@gmail.com>
2022-04-07 21:13:07 -0300

    termine el tp de hacer un get a una api

--- get de una api python/codigo.py
+++ get de una api python/codigo.py
@@ -2,7 +2,9 @@ import requests
 
 url = "https://rickandmortyapi.com/api/character/5"
 response = requests.get(url)
+file1 = open("file1.py", "w") 
+file1.write(response.text)
+file1.close()
 
 print(response.text)
-
 input()
