6092fedf8d5968e1597baf8434ba4404f00f7f4b
anchart <valenanchart@gmail.com>
2022-11-17 22:27:41 -0300

    termine el api a la calculadora y a la cuadratica (Se encuentra en Java/API/api a calculadora/)

--- Java/API/api a calculadora/calculadoraapi/src/test/java/com/example/calculadoraapi/CalculadoraapiApplicationTests.java
+++ Java/API/api a calculadora/calculadoraapi/src/test/java/com/example/calculadoraapi/CalculadoraapiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.calculadoraapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CalculadoraapiApplicationTests {
 
 	@Test
 	void contextLoads() {
